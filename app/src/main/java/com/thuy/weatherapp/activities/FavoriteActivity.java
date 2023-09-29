0acac9cf301df9eab636d00365913a8140b6d1bd
Thuy9906 <tnganthuy@yahoo.fr>
2023-09-29 13:56:20 +0200

    TP4: Add favorite TODO UI favorite list

--- app/src/main/java/com/thuy/weatherapp/activities/FavoriteActivity.java
+++ app/src/main/java/com/thuy/weatherapp/activities/FavoriteActivity.java
@@ -23,9 +23,18 @@ import com.thuy.weatherapp.R;
 import com.thuy.weatherapp.adapters.FavoriteAdapter;
 import com.thuy.weatherapp.models.City;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.thuy.weatherapp.databinding.ActivityFavoriteBinding;
+
+import org.json.JSONException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class FavoriteActivity extends AppCompatActivity {
 
@@ -60,14 +69,14 @@ public class FavoriteActivity extends AppCompatActivity {
         binding.textViewFromExtra.setText(strMessage);
 
         mCities = new ArrayList<>();
-        City city1 = new City("Montréal", "Légères pluies", "22°C", R.drawable.weather_rainy_grey);
-        City city2 = new City("New York", "Ensoleillé", "22°C", R.drawable.weather_sunny_grey);
-        City city3 = new City("Paris", "Nuageux", "24°C", R.drawable.weather_foggy_grey);
-        City city4 = new City("Toulouse", "Pluies modérées", "20°C", R.drawable.weather_rainy_grey);
-        mCities.add(city1);
-        mCities.add(city2);
-        mCities.add(city3);
-        mCities.add(city4);
+//        City city1 = new City("Montréal", "Légères pluies", "22°C", R.drawable.weather_rainy_grey);
+//        City city2 = new City("New York", "Ensoleillé", "22°C", R.drawable.weather_sunny_grey);
+//        City city3 = new City("Paris", "Nuageux", "24°C", R.drawable.weather_foggy_grey);
+//        City city4 = new City("Toulouse", "Pluies modérées", "20°C", R.drawable.weather_rainy_grey);
+//        mCities.add(city1);
+//        mCities.add(city2);
+//        mCities.add(city3);
+//        mCities.add(city4);
 
         mContext = this;
 
@@ -88,14 +97,55 @@ public class FavoriteActivity extends AppCompatActivity {
         builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+
+                retrieveCityJsonFromApiRequest(editTextCity.getText().toString());
+
                 Log.d("TAG", "confirmer");
-                City city = new City(editTextCity.getText().toString(), "Ensoleillé", "28°C", R.drawable.weather_sunny_grey);
-                mCities.add(city);
-                mAdapter.notifyDataSetChanged();
+
             }
         });
         builder.setNegativeButton(android.R.string.cancel, null);
         builder.create().show();
+    }
+
+    public void retrieveCityJsonFromApiRequest(String cityName) {
+
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=01897e497239c8aff78d9b8538fb24ea&units=metric&lang=fr").build();
+        new OkHttpClient().newCall(request).enqueue(new Callback() {
+                                                        @Override
+                                                        public void onFailure(Call call, IOException e) {
+                                                        }
+
+                                                        @Override
+                                                        public void onResponse(Call call, Response response) throws IOException {
+                                                            if (response.isSuccessful()) {
+
+//                                                           String strJson = response.body().string();
+//                                                           Log.d("TAG", "retrieveCityJsonFromApiRequest" + strJson);
+                                                                final String strJson = response.body().string();
+                                                                runOnUiThread(new Runnable() {
+                                                                    @Override
+                                                                    public void run() {
+                                                                        updateUi(strJson);
+                                                                    }
+                                                                });
+                                                            }
+                                                        }
+                                                    }
+        );
+
+    }
+
+    private void updateUi(String strJson) {
+        City city = null;
+        try {
+            city = new City(strJson);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        mCities.add(city);
+        mAdapter.notifyDataSetChanged();
     }
 
 //    public void onDestroy() {
